683a198ddb4133183f364118512676ffd637ee77
aldairandre <aldairandre99@gmail.com>
2022-06-18 13:59:08 +0100

    add firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import MyRoutes from './routers'
 import GlobalStyle from './globalStyle'
+import { connectFirestoreEmulator } from 'firebase/firestore';
+import { connectFunctionsEmulator } from 'firebase/functions';
+import { connectAuthEmulator  } from 'firebase/auth';
+import { authApp, db, functions } from './firebase/config';
 
-const App = () => {
+const App = () => { 
+  if(window.location.hostname === 'localhost'){
+    connectFirestoreEmulator(db,'localhost',8080)
+    connectFunctionsEmulator(functions,'localhost',5001)
+    connectAuthEmulator(authApp,'http://localhost:9099')
+  }
   return (
     <>
       <GlobalStyle />
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,2 +1,19 @@
-import { getApp, initializeApp } from 'firebase/app';
+import { getApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getFunctions} from "firebase/functions"
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAfrMUwpAVXLRIG4C3ENBWeVcd1oQW3RAk",
+  authDomain: "note-app-be725.firebaseapp.com",
+  projectId: "note-app-be725",
+  storageBucket: "note-app-be725.appspot.com",
+  messagingSenderId: "385139230586",
+  appId: "1:385139230586:web:a36d52d0e5beac647578c3"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
+export const functions = getFunctions(getApp())
+export const authApp = getAuth()
