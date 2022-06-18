b1c497a7ca0f37cabe2d353170acc3f7e9992275
aldairandre <aldairandre99@gmail.com>
2022-06-18 08:27:49 +0100

    pagina de login do adm

--- src/pages/Login-admin.tsx
+++ src/pages/Login-admin.tsx
@@ -1,8 +1,40 @@
+import { useState } from 'react'
 import NavBar from '../components/NavBar'
+import { Form, Title } from './SignUp'
+
 const LoginAdmin = () => {
+    const [formLogin, setFormLogin] = useState({})
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+        const dataForm = Object.fromEntries(formData)
+        console.log(dataForm);
+    }
     return (
         <>
-            <NavBar/>   
+            <NavBar/> 
+            <Title>Login</Title>  
+            <Form onSubmit={handleSubmit}>
+               <div>
+                    <label htmlFor="">Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Insira seu email"
+                    />
+               </div>
+               <div>
+                   <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Insira sua password"
+                    />
+               </div>
+               <input type="submit" value="Login" />
+            </Form>
         </>
     )
 }
