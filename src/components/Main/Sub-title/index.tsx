fc18a7b473a309c92874eea22b7d52101def2482
aldairandre <aldairandre99@gmail.com>
2022-11-11 01:21:38 +0000

    💄 (ADD TEXT): Addint text

--- src/components/Main/Sub-title/index.tsx
+++ src/components/Main/Sub-title/index.tsx
@@ -1,19 +1,21 @@
 import styled from "styled-components";
-interface props {
+export interface props {
   text: string;
   mt: number;
   mb: number;
+  color: string;
 }
-const SubTitle = ({ text, mt, mb }: props) => (
-  <H2 mt={mt} mb={mb}>
+const SubTitle = ({ text, mt, mb, color }: props) => (
+  <H2 mt={mt} mb={mb} color={color}>
     {text}
   </H2>
 );
 
-const H2 = styled.h2<{ mt: number; mb: number }>`
+export const H2 = styled.h2<{ mt: number; mb: number; color: string }>`
   margin-top: ${(props) => props.mt}px;
   margin-bottom: ${(props) => props.mb}px;
   font-size: 14.05px;
+  color: ${(props) => props.color || "white"};
 `;
 
-export default SubTitle;
+export { SubTitle };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 import StoryPost from "./Story-Card";
-import SubTitle from "./Sub-title";
+import {SubTitle,props} from "./Sub-title";
 import Title from "./title";
 export const Main = () => {
   return (
     <Container>
       <Title />
-      <SubTitle text={"Posts em destaque"} mt={18} mb={12} />
+      <SubTitle text={"Posts em destaque"} mt={18} mb={12} color=''/>
       <ContainerPost>
         <StoryPost />
         <StoryPost />
       </ContainerPost>
+      <SubTitle text={"Todos os Posts"} mt={18} mb={11} color=''/>
+      <SubTitle text={"Filtar posts por"} mt={0} mb={10} color='#BCB5B5'/>
     </Container>
   );
 };
@@ -27,6 +29,7 @@ const ContainerPost = styled.div`
   display: flex;
   flex-direction: row;
   column-gap: 20px;
+  overflow-x: scroll;
 `;
 
 export default Main;
